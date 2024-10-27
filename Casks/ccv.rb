c5f2dc343620c7b09217058f9d9fe7abcdb74c5f
Andrei Sirotenko <panshirotenski@gmail.com>
2024-10-27 05:19:06 +0300

    fix typo

--- Casks/ccv.rb
+++ Casks/ccv.rb
@@ -1,13 +1,13 @@
 cask "ccv" do
   on_arm do
     version "0.2.3"
-    sha256 "3aa991b8b5edea30b0fbfcb2e3a45b9f866db3a9cb8c6de5fc3f280e82f61512  "
+    sha256 "3aa991b8b5edea30b0fbfcb2e3a45b9f866db3a9cb8c6de5fc3f280e82f61512"
 
     url "https://github.com/ansirotenko/ccv/releases/download/ccv-v#{version}/ccv_#{version}_aarch64.dmg"
   end
   on_intel do
     version "0.2.2"
-    sha256 "301578d0d20aba553ed62f3f04aa5824300d7856467eed722a6abf438d3e3c4e  "
+    sha256 "301578d0d20aba553ed62f3f04aa5824300d7856467eed722a6abf438d3e3c4e"
 
     url "https://github.com/ansirotenko/ccv/releases/download/ccv-v#{version}/ccv_#{version}_x64.dmg"
   end
