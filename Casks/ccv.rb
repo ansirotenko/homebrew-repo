2aba8e6f0c7971728f2b38cb88702fb8301cdaed
Andrei Sirotenko <panshirotenski@gmail.com>
2025-01-25 01:40:51 +0300

    0.2.7

--- Casks/ccv.rb
+++ Casks/ccv.rb
@@ -1,13 +1,13 @@
 cask "ccv" do
   on_arm do
-    version "0.2.6"
-    sha256 "b5d7ef84636d044f8f4844314e44e15e9972e504392f5238e6ca1ad566fe23f8"
+    version "0.2.7"
+    sha256 "923cc745a984e02420086d32c13f58093ac56ae8be4dd077839fc907cebcdbe6"
 
     url "https://github.com/ansirotenko/ccv/releases/download/ccv-v#{version}/ccv_#{version}_aarch64.dmg"
   end
   on_intel do
-    version "0.2.6"
-    sha256 "8e757ff4895b2391a614ad2ffe4f827c98e8269924260d0ffec86273eb0be51d"
+    version "0.2.7"
+    sha256 "75d5eb7a9a0c1cd7013e883e809c353c1454688648e1ead65a43769813a75ca1"
 
     url "https://github.com/ansirotenko/ccv/releases/download/ccv-v#{version}/ccv_#{version}_x64.dmg"
   end
